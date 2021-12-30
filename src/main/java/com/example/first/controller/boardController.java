95ada7be4e4ddf46e742b50b9d4fadf59b20bb61
trackcrash <auroknh@gmail.com>
2021-12-30 19:56:46 +0900

    12_30 write complete modify test

--- src/main/java/com/example/first/controller/boardController.java
+++ src/main/java/com/example/first/controller/boardController.java
@@ -2,11 +2,14 @@ package com.example.first.controller;
 
 import com.example.first.model.board;
 import com.example.first.repository.BoardRepository;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -14,17 +17,35 @@ import java.util.List;
 public class boardController {
     @Autowired
     private BoardRepository boardRepository;
-
-    @RequestMapping("/list")
+    // 글 목록
+    @GetMapping("/list")
     public String list(Model model) {
         List<board> boards = boardRepository.findAll();
         model.addAttribute( "boards", boards);
         return "board/list";
     }
-
-    @RequestMapping("/write")
-    public String write() {
+    // 새 글 작성 화면
+    @GetMapping("/write")
+    public String write(Model model, @RequestParam(required = false) Long id) {
+        // 새 글 쓰기
+        if (id == null) {
+        model.addAttribute("board", new board());
+        }
+        // 수정일 경우
+        else {
+            board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
         return "board/write";
+    }
+    // 글 저장
+    @PostMapping("/write")
+    public String writeSubmit(@Valid board board, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "board/write";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 
     @RequestMapping("/view")
--- src/main/java/com/example/first/model/board.java
+++ src/main/java/com/example/first/model/board.java
@@ -2,10 +2,14 @@ package com.example.first.model;
 
 import lombok.Data;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -13,6 +17,12 @@ public class board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 50)
     private String title;
+
+    @NotNull
+    @Size(min = 2, max = 50)
     private String contents;
 }
